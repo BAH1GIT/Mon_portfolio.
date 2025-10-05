007666d099e0e16ae530e8b6a9758586642a5fb6
BAH1GIT <diabah021@gmail.com>
2025-10-05 12:26:34 +0000

    ajout de la responsivite

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 // console.log("salut");
 // alert('salut')
+
+// const openMenu =()=> {
+// const  menu =document.querySelector(".menu")
+// // menu.classList.toggle('acti')
+// menu.style.display-"flex"
+// }
+
+
+const burge = document.querySelector('.btn')
+        const burgeIcon = document.querySelector('.burger i')
+        const links = document.querySelector('.menu')
+
+        burge.onclick = function(){
+            links.classList.toggle('open')
+
+            if (links.classList.contains('open')){
+                document.querySelector('.material-icons').innerHTML='close'
+            }
+            else{
+                document.querySelector('.material-icons').innerHTML='menu'
+            }
+        }
